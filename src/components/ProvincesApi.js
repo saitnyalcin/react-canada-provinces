1c437b1d450cd6b1329d96c72e61839df6dde8b4
steveyalcin <steven.yalcin@gmail.com>
2020-03-10 01:03:11 -0300

    The details of the province will be updated inside the details div once the related province is selected

--- src/components/ProvincesApi.js
+++ src/components/ProvincesApi.js
@@ -4,19 +4,28 @@ import axios from 'axios';
 export default function ProvinceApi() {
   const [data, setData] = useState([]);
 
+  var ApiRequest =
+    'https://raw.githubusercontent.com/Clavicus/Testing-Requests/master/canadian-provinces.json';
+
   useEffect(() => {
-    axios
-      .get(
-        'https://raw.githubusercontent.com/Clavicus/Testing-Requests/master/canadian-provinces.json'
-      )
-      .then(result => setData(result.data));
-  }, []);
+    axios.get(ApiRequest).then(result => setData(result.data));
+  }, [ApiRequest]);
 
   return (
-    <div className="flex-container">
-      {data.map(item => (
-        <div key={item.name}>{item.name}</div>
-      ))}
+    <div className="main-flex">
+      <div className="flex-container">
+        {data.map(item => (
+          <div key={item.name}>{item.name}</div>
+        ))}
+      </div>
+
+      <div className="province-details">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+      </div>
     </div>
   );
 }
