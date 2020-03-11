c3c4278b0c6a8ee92a59b13fcb673112c4f3e6d5
steveyalcin <steven.yalcin@gmail.com>
2020-03-10 22:56:19 -0300

    The state of the province values are set and will be updated for the future state

--- src/components/ProvincesApi.js
+++ src/components/ProvincesApi.js
@@ -2,8 +2,15 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function ProvinceApi() {
+  // holds the state value of the provinces
   const [data, setData] = useState([]);
+  // set the updated state of the details of the hovered province in the list
+  const [capital, setCapital] = useState('Alberta');
+  const [population, setPopulation] = useState('4067175');
+  const [latitude, setLatitude] = useState('53.9332706');
+  const [longitude, setLongtitude] = useState('-116.5765035');
 
+  // assingn the static url of the api
   var ApiRequest =
     'https://raw.githubusercontent.com/Clavicus/Testing-Requests/master/canadian-provinces.json';
 
@@ -20,11 +27,11 @@ export default function ProvinceApi() {
       </div>
 
       <div className="province-details">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
+        <div>Capital : {capital}</div>
+        <div>Population : {population}</div>
+        <div>
+          Latitude : {latitude} | Longitude : {longitude}
+        </div>
       </div>
     </div>
   );
