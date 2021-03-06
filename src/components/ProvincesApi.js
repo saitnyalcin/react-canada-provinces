a6b66d24c7efe4fae67ee95b0a86e4e8621b444d
Steve Yalcin <steve.yalcin@ibm.com>
2021-03-06 15:29:21 -0500

    axios lib is removed and code base modification

--- src/components/ProvincesApi.js
+++ src/components/ProvincesApi.js
@@ -1,43 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Province from './Province';
-import Capitals from './Capitals';
-import Population from './Population';
-import Coordinates from './Coordinates';
+import React, { useEffect, useState } from "react";
+import Capitals from "./Capitals";
+import Coordinates from "./Coordinates";
+import Population from "./Population";
+import Province from "./Province";
 
 export default function ProvinceApi() {
   // holds the state value of the provinces
   const [data, setData] = useState([]);
 
   // using axios data fetching library to retrieve the data from api end point
-  const fetch = () => {
-    axios
-      .get(
-        'https://raw.githubusercontent.com/Clavicus/Testing-Requests/master/canadian-provinces.json'
-      )
-      .then(result => setData(result.data));
-  };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  async function callApiFunc() {
+    await fetch(
+      "https://raw.githubusercontent.com/Clavicus/Testing-Requests/master/canadian-provinces.json"
+    )
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .then((result) => {
+        console.log("Success:", result);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 
   useEffect(() => {
-    fetch();
+    callApiFunc();
   }, []);
 
   return (
     <div className="main-flex">
       <div className="flex-container">
-        {data.map(province => (
+        {data.map((province) => (
           <Province key={province.name} province={province.name} />
         ))}
       </div>
 
       <div className="capital">
-        {data.map(capital => (
+        {data.map((capital) => (
           <Capitals key={capital.name} capital={capital.capital} />
         ))}
       </div>
 
       <div className="population">
-        {data.map(population => (
+        {data.map((population) => (
           <Population
             key={population.name}
             population={population.population}
@@ -46,7 +52,7 @@ export default function ProvinceApi() {
       </div>
 
       <div className="coordinates">
-        {data.map(coordinate => (
+        {data.map((coordinate) => (
           <Coordinates
             key={coordinate.name}
             latitude={coordinate.latitude}
